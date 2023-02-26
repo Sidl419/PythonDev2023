4ba5a97c380c68a7938c99910c2375598e755c9d
Сидоров Леонид Станиславович <leonid.sidorov@sbermarket.ru>
2023-02-26 18:51:02 +0300

    added ask and inform functions for task 3

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -1,15 +1,34 @@
 from collections import defaultdict
+from typing import Optional
+
 
 def bullscows(guess: str, secret: str) -> tuple[int, int]:
     bulls, cows = 0, 0
+
     secret_chars = defaultdict(int)
     for c in secret:
         secret_chars[c] += 1
-    for i in range(len(guess)):
-        if i < len(secret) and guess[i] == secret[i]:
+
+    for i, guess_char in enumerate(guess):
+        if (i < len(secret)) and (guess_char == secret[i]):
             bulls += 1
-            secret_chars[guess[i]] -= 1
-        elif secret_chars[guess[i]]:
-            secret_chars[guess[i]] -= 1
+            secret_chars[guess_char] -= 1
+        elif secret_chars[guess_char]:
             cows += 1
+            secret_chars[guess_char] -= 1
+            
     return bulls, cows
+
+
+def ask(prompt: str, valid: Optional[list[str]] = None) -> str:
+    guess = input(prompt)
+
+    if not valid:
+        while guess not in valid:
+            guess = input(prompt)
+
+    return guess
+
+
+def inform(format_string: str, bulls: int, cows: int) -> None:
+    print(format_string.format(bulls, cows))
