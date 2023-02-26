699ab28708ffe6a80a4c431f6714313cf4883b39
Сидоров Леонид Станиславович <leonid.sidorov@sbermarket.ru>
2023-02-26 12:33:56 +0300

    added main cowsay functionality

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -1,6 +1,38 @@
+from cowsay import cowsay
 import argparse
+import sys
+
 
 OPTIONALS = set("bdgpstwy")
+
+
+def main(args):
+    input_lines = []
+    for line in sys.stdin:
+        input_lines.append(line.strip())
+    input_lines = "\n".join(input_lines)
+    
+    preset = None
+    for option, value in args._get_kwargs():
+        if option in OPTIONALS and value:
+            preset = option
+            break
+
+    is_wrap = not args.n
+
+    print(
+        cowsay(
+            message=input_lines,
+            cow=args.f,
+            preset=preset,
+            eyes=args.e,
+            tongue=args.T,
+            width=args.W,
+            wrap_text=is_wrap
+        ), 
+        file=sys.stdout
+    )
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='my cowsay wrapper')
@@ -16,4 +48,4 @@ if __name__ == '__main__':
         parser.add_argument(f"-{option}", action="store_true")
     
     args = parser.parse_args()
-    print(args)
+    main(args)
