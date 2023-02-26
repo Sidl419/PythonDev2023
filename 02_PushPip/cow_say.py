77bfe387c97b52997f41fc575fcde3e11bcab219
Сидоров Леонид Станиславович <leonid.sidorov@sbermarket.ru>
2023-02-26 12:25:56 +0300

    added optional arguments

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -1,15 +1,19 @@
 import argparse
 
+OPTIONALS = set("bdgpstwy")
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser(description='my cowsay wrapper')
+
     parser.add_argument("-e", type=str, default='oo', help="appearance of the cow's eyes")
     parser.add_argument("-f", type=str, default='default', help="a particular cow picture file to use")
     parser.add_argument("-l", action="store_true", help="list all cowfiles on the current COWPATH")
     parser.add_argument("-n", action="store_true", help="if specified, the given message will not be wrapped")
     parser.add_argument("-T", type=str, default='  ', help="appearance of the cow's tongue")
     parser.add_argument("-W", type=int, default=40, help="specifies where the message should be wrapped")
+
+    for option in OPTIONALS:
+        parser.add_argument(f"-{option}", action="store_true")
     
     args = parser.parse_args()
     print(args)
