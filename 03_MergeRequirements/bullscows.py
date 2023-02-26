a109be6062a173fd3f9e3969bb20b77e1b4df5bb
Сидоров Леонид Станиславович <leonid.sidorov@sbermarket.ru>
2023-02-26 18:57:06 +0300

    added gameplay function for task 3

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from typing import Optional
+from random import choice
 
 
 def bullscows(guess: str, secret: str) -> tuple[int, int]:
@@ -32,3 +33,20 @@ def ask(prompt: str, valid: Optional[list[str]] = None) -> str:
 
 def inform(format_string: str, bulls: int, cows: int) -> None:
     print(format_string.format(bulls, cows))
+
+
+def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
+    secret = choice(words)
+    count = 0
+
+    while True:
+        count += 1
+        guess = ask("Введите слово: ", words)
+
+        bulls, cows = bullscows(guess, secret)
+        inform("Быки: {}, Коровы: {}", bulls, cows)
+
+        if bulls == len(secret):
+            break
+        
+    return count
