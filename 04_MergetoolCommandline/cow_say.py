0f6e62860fa998ebf35cdd6dc11b60a6a8b81a72
Сидоров Леонид Станиславович <leonid.sidorov@sbermarket.ru>
2023-03-06 03:37:45 +0300

    add make_bubble function

--- 04_MergetoolCommandline/cow_say.py
+++ 04_MergetoolCommandline/cow_say.py
@@ -1,6 +1,6 @@
 import cmd
 import shlex
-from cowsay import cowsay, list_cows, cowthink
+from cowsay import cowsay, list_cows, cowthink, make_bubble, THOUGHT_OPTIONS
 
 
 class CowCmd(cmd.Cmd):
@@ -61,6 +61,31 @@ class CowCmd(cmd.Cmd):
                 params[add_param] = param
                 add_param = ''
         print(cowthink(params['message'], cow=params['cow'], eyes=params['eyes'], tongue=params['tongue']))
+
+    def make_bubble(self, arg):
+        '''
+        arguments: 
+            wrap_text=True: wraps text if is true
+            width=40: wrapper width
+            brackets=THOUGHT_OPTIONS["cowsay"]: bubble type
+
+        Pads text and sets inside a bubble. This is the text that appears above the cows
+        '''
+        input_str = shlex.split(arg)
+        params = {"message": input_str[0], "brackets": THOUGHT_OPTIONS['cowsay'], "width" : 40, "wrap_text" : False}
+        add_param = ''
+        for param in input_str[1:]:
+            if param.startswith("--") and (param[2:] in params):
+                add_param = param[2:]
+            elif len(add_param):
+                if add_param == 'brackets':
+                    params[add_param] = THOUGHT_OPTIONS[param]
+                elif add_param == 'wrap_text':
+                    params[add_param] = param == "true"
+                else:
+                    params[add_param] = param
+                add_param = ''
+        print(make_bubble(params['message'], brackets=params['brackets'], width=params['width'], wrap_text=params['wrap_text']))
 
     def exit(self, arg):
         'exit from cow cmd'
