488665896d8e9dfb885ce4e9b0fb8637b5f7888b
Сидоров Леонид Станиславович <leonid.sidorov@sbermarket.ru>
2023-03-13 01:43:57 +0300

    added chat template

---
+++ 05_DiffPatchNet/cow_chat.py
@@ -0,0 +1,35 @@
+import asyncio
+
+clients = {}
+
+async def chat(reader, writer):
+    me = "{}:{}".format(*writer.get_extra_info('peername'))
+    print(me)
+    clients[me] = asyncio.Queue()
+    send = asyncio.create_task(reader.readline())
+    receive = asyncio.create_task(clients[me].get())
+    while not reader.at_eof():
+        done, pending = await asyncio.wait([send, receive], return_when=asyncio.FIRST_COMPLETED)
+        for q in done:
+            if q is send:
+                send = asyncio.create_task(reader.readline())
+                for out in clients.values():
+                    if out is not clients[me]:
+                        await out.put(f"{me} {q.result().decode().strip()}")
+            elif q is receive:
+                receive = asyncio.create_task(clients[me].get())
+                writer.write(f"{q.result()}\n".encode())
+                await writer.drain()
+    send.cancel()
+    receive.cancel()
+    print(me, "DONE")
+    del clients[me]
+    writer.close()
+    await writer.wait_closed()
+
+async def main():
+    server = await asyncio.start_server(chat, '0.0.0.0', 1337)
+    async with server:
+        await server.serve_forever()
+
+asyncio.run(main())
