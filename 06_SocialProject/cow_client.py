c6a9f5c932df877e0dc5ec658e59698d44f2bc7f
Сидоров Леонид Станиславович <leonid.sidorov@sbermarket.ru>
2023-03-26 16:56:32 +0300

    added exit function

--- 06_SocialProject/cow_client.py
+++ 06_SocialProject/cow_client.py
@@ -10,16 +10,25 @@ lock = threading.Lock()
 class CowClient(cmd.Cmd):
 
     def do_who(self, args):
+        'list available cows from the server'
         s.send("who\n".encode())
+
+    def do_exit(self, args):
+        'exit from cow client'
+        s.send("exit\n".encode())
+        return 1
 
 
 def messenger(cmdline):
-    while True:
-        with lock:
-            msg = s.recv(1024).decode()
-        if msg:
-            print(msg.strip())
-            print(f"{cmdline.prompt}{readline.get_line_buffer()}", end="", flush=True)
+    try:
+        while True:
+            with lock:
+                msg = s.recv(1024).decode()
+            if msg:
+                print(msg.strip())
+                print(f"{cmdline.prompt}{readline.get_line_buffer()}", end="", flush=True)
+    except socket.error as e:
+        print("your session is closed")
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
